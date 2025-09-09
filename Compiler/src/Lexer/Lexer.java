efe5e70bb57d1e37b534abc68c2be9cf0b43089d
Damir <damir.sadykov0407@gmail.com>
2025-09-09 22:20:30 +0300

    feat: methods to work with chars and strings

--- Compiler/src/Lexer/Lexer.java
+++ Compiler/src/Lexer/Lexer.java
@@ -1,13 +1,69 @@
 package Lexer;
 
 public class Lexer {
+    static String fileText;
+    static int filePointer = -1;
+
     public static void main(String[] args) {
         // TODO: Args check
     }
 
-    char get() {
-        // TODO: get next char from file
-        return '0';
+    static char nextChar() {
+        filePointer += 1;
+        return fileText.charAt(filePointer);
+    }
+
+    static String nextWord() {
+        StringBuilder word = new StringBuilder();
+        boolean stop = false;
+        while (true) {
+            char nextChar = nextChar();
+            switch (nextChar) {
+                case ' ':
+                case '.':
+                    stop = true;
+                    backChar();
+                    break;
+                default:
+                    word.append(nextChar);
+                    break;
+            }
+            if (stop) {
+                break;
+            }
+        }
+        return word.toString();
+    }
+
+    static char backChar() {
+        if (filePointer != 0)
+            filePointer -= 1;
+        return fileText.charAt(filePointer);
+    }
+
+    static void backWord() {
+        boolean stop = false;
+        while (true) {
+            if (filePointer == 0) {
+                filePointer = -1;
+                break;
+            }
+            switch (backChar()) {
+                case ' ':
+                case '.':
+                    stop = true;
+                    nextChar();
+                    break;
+            }
+            if (stop) {
+                break;
+            }
+        }
+    }
+
+    static String fullWord() {
+        backWord();
+        return nextWord();
     }
 
     void process() {
