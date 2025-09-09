d8bf2b186fd6901be0d0650c14e9ea60568a2ac8
Damir <damir.sadykov0407@gmail.com>
2025-09-09 21:49:15 +0300

    Initial lexer commit

---
+++ Compiler/src/Lexer/Lexer.java
@@ -0,0 +1,21 @@
+package Lexer;
+
+public class Lexer {
+    public static void main(String[] args) {
+        // TODO: Args check
+    }
+
+    char get() {
+        // TODO: get next char from file
+        return '0';
+    }
+
+    void process() {
+        // TODO: Cycle to process file
+    }
+
+    Token nexToken() {
+        // TODO: FSM like switch to construct token
+        return null;
+    }
+}
