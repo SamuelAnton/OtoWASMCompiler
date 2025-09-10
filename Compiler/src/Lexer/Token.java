e095de85db280edab0ffcecb213aa9ec8be58ace
Adel Gaznanov <a.gaznanov@innopolis.university>
2025-09-10 22:28:16 +0300

    refactor: create additional tokens

--- Compiler/src/Lexer/Token.java
+++ Compiler/src/Lexer/Token.java
@@ -16,6 +16,7 @@ public enum Token {
     tkWhile("while"), // while
     tkLoop("loop"), // loop
     tkIf("if"), // if
+    tkElse("else"), // else
     tkOpenCircle("("), // (
     tkCloseCircle(")"), // )
     tkOpenSquare("["), // [
