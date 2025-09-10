dc546af7680cbe26bf559fe21bcbabbefb15757a
Adel Gaznanov <a.gaznanov@innopolis.university>
2025-09-10 22:40:17 +0300

    refactor: create additional tokens

--- Compiler/src/Lexer/App.java
+++ Compiler/src/Lexer/App.java
@@ -1,12 +1,26 @@
 package Lexer;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
         // TODO: Args check in another class
+        StringBuilder builder = new StringBuilder();
         Lexer l = new Lexer();
-        ArrayList<Token> tokens = l.process("class  end   class");
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        String inputString = builder.toString();
+        ArrayList<Token> tokens = l.process(inputString);
         for (int i = 0; i < tokens.size(); i++) {
             System.out.println(tokens.get(i));
         }
