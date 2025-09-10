df44bb38b6bf8f88f4133c789846fd13ba48f51d
Adel Gaznanov <a.gaznanov@innopolis.university>
2025-09-10 22:15:10 +0300

    refactor: create additional tokens

--- Compiler/src/Lexer/Token.java
+++ Compiler/src/Lexer/Token.java
@@ -1,15 +1,22 @@
 package Lexer;
 
 public enum Token {
-    tkIdentifier, // user's var
-    tkClass, // class
-    tkIs, // is
-    tkEnd, // end
-    tkReturn, // return
-    tkThis, // this
-    tkMethod, // method
-    tkShortBody, // =>
-    tkExtends, // extends
-    tkAssignment, // : Example: (var x : Animal())
-    tkCall, // . Example: c.get()
+    tkIdentifier("var"), // user's var
+    tkClass("class"), // class
+    tkIs("is"), // is
+    tkEnd("end"), // end
+    tkReturn("return"), // return
+    tkThis("this"), // this
+    tkMethod("method"), // method
+    tkShortBody("=>"), // =>
+    tkExtends("extends"), // extends
+    tkAssignment1(":"), // : Example: (var x : Animal())
+    tkCall("."), // . Example: c.get()
+    tkAssignment2(":="), // := Example: n := v
+
+    ;
+    public final String label;
+    Token(String label) {
+        this.label = label;
+    }
 }
