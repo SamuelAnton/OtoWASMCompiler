648531b3c98d6032f6a516f2485a6ab1234dfdb8
Damir <damir.sadykov0407@gmail.com>
2025-11-05 20:00:05 +0300

    fix: done class member analyzer

--- Compiler/src/Semanticer/Components/Checkers/ClassMemberAnalyzer.java
+++ Compiler/src/Semanticer/Components/Checkers/ClassMemberAnalyzer.java
@@ -1,15 +1,45 @@
 package Semanticer.Components.Checkers;
 
-import Syntaxer.ast.declaration.ClassDeclaration;
+import java.util.ArrayList;
+
+import Semanticer.Components.Exceptions.ValidationException;
+import Syntaxer.ast.declaration.*;
+import Syntaxer.ast.declaration.ConstructorDeclaration;
 
 public class ClassMemberAnalyzer {
-    private ClassDeclaration cls;
+    private final ClassDeclaration cls;
+    private final ArrayList<ConstructorDeclaration> constructors = new ArrayList<>();
+    private final ArrayList<FieldDeclaration> fields = new ArrayList<>();
+    private final ArrayList<MethodDeclaration> methods = new ArrayList<>();
 
     public ClassMemberAnalyzer(ClassDeclaration c) {
         cls = c;
     }
 
     public void analyze() {
+        for (MemberDeclaration m : cls.members) {
+            if (m instanceof ConstructorDeclaration) {
+                constructors.add((ConstructorDeclaration) m);
+            } else if (m instanceof FieldDeclaration) {
+                fields.add((FieldDeclaration) m);
+            } else if (m instanceof MethodDeclaration) {
+                methods.add((MethodDeclaration) m);
+            } else {
+                throw new ValidationException(
+                        "Cannot resolve class member to any existing ones (constructor, field, method).");
+            }
+        }
+    }
 
+    public ArrayList<ConstructorDeclaration> getConstructors() {
+        return constructors;
+    }
+
+    public ArrayList<FieldDeclaration> getFields() {
+        return fields;
+    }
+
+    public ArrayList<MethodDeclaration> getMethods() {
+        return methods;
     }
 }
