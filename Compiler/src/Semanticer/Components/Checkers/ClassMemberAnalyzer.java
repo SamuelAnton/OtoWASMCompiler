6dec14de1d5eeaf384b8585b52e1b82a98bd1eff
Damir <damir.sadykov0407@gmail.com>
2025-11-05 19:38:14 +0300

    feat: class member analysis

---
+++ Compiler/src/Semanticer/Components/Checkers/ClassMemberAnalyzer.java
@@ -0,0 +1,15 @@
+package Semanticer.Components.Checkers;
+
+import Syntaxer.ast.declaration.ClassDeclaration;
+
+public class ClassMemberAnalyzer {
+    private ClassDeclaration cls;
+
+    public ClassMemberAnalyzer(ClassDeclaration c) {
+        cls = c;
+    }
+
+    public void analyze() {
+
+    }
+}
