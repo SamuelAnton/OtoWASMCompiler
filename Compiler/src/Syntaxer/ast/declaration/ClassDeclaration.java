42bcf3753272cd6929a0ba2927012c79452065f5
Adel Gaznanov <a.gaznanov@innopolis.university>
2025-11-29 19:32:28 +0300

    feat: create methodsList and fieldsList in ClassDeclaration

--- Compiler/src/Syntaxer/ast/declaration/ClassDeclaration.java
+++ Compiler/src/Syntaxer/ast/declaration/ClassDeclaration.java
@@ -4,10 +4,10 @@ import Syntaxer.ast.ASTNode;
 import Syntaxer.ast.ASTVisitor;
 import Syntaxer.ast.component.ExtensionType;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import Semanticer.Components.Types.VariableType;
+import Syntaxer.ast.component.Param;
 
 public class ClassDeclaration extends ASTNode {
     public final String name;
@@ -17,6 +17,9 @@ public class ClassDeclaration extends ASTNode {
     public final List<MethodDeclaration> methodDeclarations = new ArrayList<>();
     public final List<FieldDeclaration> fieldDeclarations = new ArrayList<>();
     public final List<ConstructorDeclaration> constructorDeclarations = new ArrayList<>();
+
+    public List<FieldDeclaration> fieldsList = new ArrayList<>();
+    public List<MethodDeclaration> methodsList = new ArrayList<>();
     public ClassDeclaration superClass;
 
     public VariableType type;
@@ -31,5 +34,40 @@ public class ClassDeclaration extends ASTNode {
     @Override
     public <R> R accept(ASTVisitor<R> visitor) {
         return visitor.visit(this);
+    }
+
+    public void fillFieldsList() {
+        if (superClass != null) {
+            fieldsList.addAll(superClass.fieldDeclarations);
+        }
+        fieldsList.addAll(fieldDeclarations);
+    }
+
+    public int getIndexByField(FieldDeclaration input) {
+        for (int i = fieldsList.size() - 1; i >= 0; i--) {
+            FieldDeclaration cur = fieldsList.get(i);
+            if (cur.name.equals(input.name) && cur.type.type.equals(input.type.type)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void fillMethodsList() {
+        if (superClass != null) {
+            methodsList.addAll(superClass.methodDeclarations);
+        }
+        methodsList.addAll(methodDeclarations);
+    }
+
+    public int getIndexByMethod(MethodDeclaration input) {
+        for (int i = methodsList.size() - 1; i >= 0; i--) {
+            MethodDeclaration cur = methodsList.get(i);
+            if (cur.name.equals(input.name) && cur.returnType.equals(input.returnType)
+                    && cur.params.equals(input.params)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
